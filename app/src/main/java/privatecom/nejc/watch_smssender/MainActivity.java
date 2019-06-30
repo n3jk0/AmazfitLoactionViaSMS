0bfa6b03d18fd1c3a31dcc94ff7f6d28db0fa45b
Nejc Kozlevčar <nejc.kozlevcar@solvera-lynx.com>
2019-06-30 22:21:37 +0200

    ConnectThread

--- app/src/main/java/privatecom/nejc/watch_smssender/MainActivity.java
+++ app/src/main/java/privatecom/nejc/watch_smssender/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -22,6 +23,7 @@ import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
 import privatecom.nejc.watch_smssender.domain.MyLocation;
 
@@ -29,7 +31,12 @@ public class MainActivity extends AppCompatActivity {
     TextView statusBar;
     LocationManager locationManager;
     Location lastLocation;
+    BluetoothAdapter bluetoothAdapter;
     volatile boolean locationChanged = false;
+
+    private final static String NAME = "bluetooth_sms";
+    private final static UUID MY_UUID = UUID.fromString(NAME);
+    private final static String TAG = "watch_smssender";
 
     private static final Map<String, MyLocation> LOCATION_MAP = new HashMap<>();
     private static final Map<String, String> ADRESS_NUMBERS = new HashMap<>();
@@ -58,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
         statusBar = findViewById(R.id.statusBar);
         setContentView(R.layout.activity_main);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 
     public void getLocationAndSendSMS(View view) {
@@ -216,6 +224,62 @@ public class MainActivity extends AppCompatActivity {
 
         for (BluetoothDevice device : pairedDevices) {
             System.out.println(device.getName() + " " + device.getAddress());
+        }
+    }
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket
+            // because mmSocket is final.
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            try {
+                // Get a BluetoothSocket to connect with the given BluetoothDevice.
+                // MY_UUID is the app's UUID string, also used in the server code.
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's create() method failed", e);
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            bluetoothAdapter.cancelDiscovery();
+
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+                return;
+            }
+
+            // The connection attempt succeeded. Perform work associated with
+            // the connection in a separate thread.
+            manageMyConnectedSocket(mmSocket);
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the client socket", e);
+            }
+        }
+
+        private void manageMyConnectedSocket(BluetoothSocket mmSocket) {
         }
     }
 }
